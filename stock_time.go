8b03db8462a23ebe69a01416319e97820acede99
Ivan Kochergin <ivan.kchg@gmail.com>
2023-01-10 10:36:08 +0300

    Fixed time in stock

---
+++ stock_time.go
@@ -0,0 +1,20 @@
+package openapi
+
+import (
+	"strings"
+	"time"
+)
+
+type StockTime struct {
+	time.Time
+}
+
+func (c *StockTime) UnmarshalJSON(b []byte) (err error) {
+	s := strings.Trim(string(b), "\"") // remove quotes
+	if s == "null" {
+		return
+	}
+
+	c.Time, err = time.Parse("2006-01-02T15:04:05.000", s)
+	return
+}
